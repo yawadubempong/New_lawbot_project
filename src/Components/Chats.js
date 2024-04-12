74977e21a6d56693f9517960bf1e13e584dde1d1
Miqael-Dev <miqael.dev@gmail.com>
2024-04-12 05:10:26 +0000

    add active chat fuction

--- src/Components/Chats.js
+++ src/Components/Chats.js
@@ -1,10 +1,10 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./CSS/ChatRoom.css";
 import { faTrashCan } from "@fortawesome/free-regular-svg-icons";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Chats = ({ chats , loadChat , deleteChat }) => {
-
+  const [activeId, setAciveId] = useState()
   useEffect(() => {
     console.log(chats);
 },[chats])
@@ -13,13 +13,16 @@ const Chats = ({ chats , loadChat , deleteChat }) => {
         <>
             {(chats.map((chat) => (
                   <div
-                    className="chat"
+                    id = {`${chats.indexOf(chat)}`}
+                    className={`chat ${activeId === chats.indexOf(chat)? "active": ""}`}
                     key={chat.id}
-                    onClick={() => {
+                    onClick={(e) => {
                       loadChat(chat.id);
+                      setAciveId(chats.indexOf(chat))
                     }}
                   >
                     <div className="chaticon-sentence">
+                      <img className="blue-dot" src={require("./Assets/Ellipse 513.png")} alt="blue dot"/>
                       <img
                         className="chatimg"
                         src={require("./Assets/icons8-comments-96.png")}
